9b1e9e4b2de7466c3fccb5013b0c8005e4bb4f1d
SynthesisOne <34944650+SynthesisOne@users.noreply.github.com>
2019-10-19 17:11:01 +0300

    Reg/Log

--- app/views/User/login.php
+++ app/views/User/login.php
@@ -1,45 +1,98 @@
-<!--start-breadcrumbs-->
-<div class="breadcrumbs">
-    <div class="container">
-        <div class="breadcrumbs-main">
-            <ol class="breadcrumb">
-                <li><a href="<?= PATH ?>">Главная</a></li>
-                <li>Вход</li>
-            </ol>
-        </div>
-    </div>
-</div>
-<!--end-breadcrumbs-->
-<!--prdt-starts-->
-<div class="prdt">
-    <div class="container">
-        <div class="prdt-top">
-            <div class="col-md-12">
-                <div class="product-one login">
-                    <div class="register-top heading">
-                        <h2>Вход</h2>
-                    </div>
 
-                    <div class="register-main">
-                        <div class="col-md-6 account-left">
-                            <form method="post" action="user/login" id="login" role="form" data-toggle="validator">
-                                <div class="form-group has-feedback">
-                                    <label for="login">Login</label>
-                                    <input type="text" name="login" class="form-control" id="login" placeholder="Login" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                </div>
-                                <div class="form-group has-feedback">
-                                    <label for="pasword">Password</label>
-                                    <input type="password" name="password" class="form-control" id="pasword" placeholder="Password" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                </div>
-                                <button type="submit" class="btn btn-default">Вход</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="animate-dropdown">
+        <!-- ========================================= BREADCRUMB ========================================= -->
+        <div id="top-mega-nav">
+            <div class="container">
+                <nav>
+                    <ul class="inline">
+                        <li class="dropdown le-dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-list"></i> shop by department
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Computer Cases & Accessories</a></li>
+                                <li><a href="#">CPUs, Processors</a></li>
+                                <li><a href="#">Fans, Heatsinks &amp; Cooling</a></li>
+                                <li><a href="#">Graphics, Video Cards</a></li>
+                                <li><a href="#">Interface, Add-On Cards</a></li>
+                                <li><a href="#">Laptop Replacement Parts</a></li>
+                                <li><a href="#">Memory (RAM)</a></li>
+                                <li><a href="#">Motherboards</a></li>
+                                <li><a href="#">Motherboard &amp; CPU Combos</a></li>
+                                <li><a href="#">Motherboard Components</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="breadcrumb-nav-holder">
+                            <ul>
+                                <li class="breadcrumb-item">
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li class="breadcrumb-item current gray">
+                                    <a href="about.html">Authentication</a>
+                                </li>
+                            </ul>
+                        </li><!-- /.breadcrumb-nav-holder -->
+                    </ul>
+                </nav>
+            </div><!-- /.container -->
+        </div><!-- /#top-mega-nav -->
+        <!-- ========================================= BREADCRUMB : END ========================================= -->
     </div>
-</div>
-<!--product-end-->
+
+
+    <main id="authentication" class="inner-bottom-md">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-6 col-md-offset-3">
+                    <section class="section sign-in inner-right-xs">
+                        <h2 class="bordered">Войти</h2>
+                        <p>Hello, Welcome to your account</p>
+
+                        <div class="social-auth-buttons">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <button class="btn-block btn-lg btn btn-facebook"><i class="fa fa-facebook"></i> Sign In with Facebook</button>
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn-block btn-lg btn btn-twitter"><i class="fa fa-twitter"></i> Sign In with Twitter</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                            <form method="post" action="user/login" id="login" role="form" data-toggle="validator" class="login-form cf-style-1">
+                            <div class="field-row">
+                                <label for="login">Login</label>
+                                <input type="text" name="login" class="le-input" id="login" placeholder="Login" required>
+                            </div><!-- /.field-row -->
+
+                            <div class="field-row">
+                                <label for="pasword">Password</label>
+                                <input type="password" name="password" class="le-input" id="pasword" placeholder="Password" required>
+                            </div><!-- /.field-row -->
+
+                            <div class="field-row clearfix">
+                                        <span class="pull-left">
+                                            <label class="content-color"><input type="checkbox" class="le-checbox auto-width inline"> <span class="bold">Remember me</span></label>
+                                        </span>
+                                <span class="pull-right">
+                                            <a href="#" class="content-color bold">Forgotten Password ?</a>
+                                        </span>
+                            </div>
+
+                            <div class="buttons-holder">
+
+                                <button type="submit" class="le-button huge">Вход</button>
+                            </div><!-- /.buttons-holder -->
+                        </form><!-- /.cf-style-1 -->
+
+                    </section><!-- /.sign-in -->
+                </div><!-- /.col -->
+
+
+
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </main>
--- app/views/User/signup.php
+++ app/views/User/signup.php
@@ -1,62 +1,99 @@
-<!--start-breadcrumbs-->
-<div class="breadcrumbs">
-    <div class="container">
-        <div class="breadcrumbs-main">
-            <ol class="breadcrumb">
-                <li><a href="<?= PATH ?>">Главная</a></li>
-                <li>Регистрация</li>
-            </ol>
-        </div>
-    </div>
-</div>
-<!--end-breadcrumbs-->
-<!--prdt-starts-->
-<div class="prdt">
-    <div class="container">
-        <div class="prdt-top">
-            <div class="col-md-12">
-                <div class="product-one signup">
-                    <div class="register-top heading">
-                        <h2>REGISTER</h2>
-                    </div>
 
-                    <div class="register-main">
-                        <div class="col-md-6 account-left">
-                            <form method="post" action="user/signup" id="signup" role="form" data-toggle="validator">
-                                <div class="form-group has-feedback">
-                                    <label for="login">Login</label>
-                                    <input type="text" name="login" class="form-control" id="login" placeholder="Login" value="<?=isset($_SESSION['form_data']['login']) ? h($_SESSION['form_data']['login']) : '';?>" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                </div>
-                                <div class="form-group has-feedback">
-                                    <label for="pasword">Password</label>
-                                    <input type="password" name="password" class="form-control" id="pasword" placeholder="Password" data-error="Пароль должен включать не менее 6 символов" data-minlength="6" value="<?=isset($_SESSION['form_data']['password']) ? h($_SESSION['form_data']['password']) : '';?>" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                                <div class="form-group has-feedback">
-                                    <label for="name">Имя</label>
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Имя" value="<?=isset($_SESSION['form_data']['name']) ? h($_SESSION['form_data']['name']) : '';?>" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                </div>
-                                <div class="form-group has-feedback">
-                                    <label for="email">Email</label>
-                                    <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="<?=isset($_SESSION['form_data']['email']) ? h($_SESSION['form_data']['email']) : '';?>" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                </div>
-                                <div class="form-group has-feedback">
-                                    <label for="address">Address</label>
-                                    <input type="text" name="address" class="form-control" id="address" placeholder="Address" value="<?=isset($_SESSION['form_data']['address']) ? h($_SESSION['form_data']['address']) : '';?>" required>
-                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
-                                </div>
-                                <button type="submit" class="btn btn-default">Зарегистрировать</button>
-                            </form>
-                            <?php if(isset($_SESSION['form_data'])) unset($_SESSION['form_data']); ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="animate-dropdown">
+    <!-- ========================================= BREADCRUMB ========================================= -->
+    <div id="top-mega-nav">
+        <div class="container">
+            <nav>
+                <ul class="inline">
+                    <li class="dropdown le-dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-list"></i> shop by department
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Computer Cases & Accessories</a></li>
+                            <li><a href="#">CPUs, Processors</a></li>
+                            <li><a href="#">Fans, Heatsinks &amp; Cooling</a></li>
+                            <li><a href="#">Graphics, Video Cards</a></li>
+                            <li><a href="#">Interface, Add-On Cards</a></li>
+                            <li><a href="#">Laptop Replacement Parts</a></li>
+                            <li><a href="#">Memory (RAM)</a></li>
+                            <li><a href="#">Motherboards</a></li>
+                            <li><a href="#">Motherboard &amp; CPU Combos</a></li>
+                            <li><a href="#">Motherboard Components</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="breadcrumb-nav-holder">
+                        <ul>
+                            <li class="breadcrumb-item">
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="breadcrumb-item current gray">
+                                <a href="about.html">Authentication</a>
+                            </li>
+                        </ul>
+                    </li><!-- /.breadcrumb-nav-holder -->
+                </ul>
+            </nav>
+        </div><!-- /.container -->
+    </div><!-- /#top-mega-nav -->
+    <!-- ========================================= BREADCRUMB : END ========================================= -->
 </div>
-<!--product-end-->
+
+
+<main id="authentication" class="inner-bottom-md">
+    <div class="container">
+        <div class="row">
+
+
+
+            <div class="col-md-6 col-md-offset-3">
+                <section class="section register inner-left-xs">
+                    <h2 class="bordered">Регистрация</h2>
+                    <p>Create your own Media Center account</p>
+
+                    <form role="form" class="register-form cf-style-1" method="post" action="user/signup" id="signup"  data-toggle="validator">
+                        <div class="field-row">
+                            <label for="login">Login</label>
+                            <input   type="text" name="login" class=" le-input"" id="login" placeholder="Login" value="<?=isset($_SESSION['form_data']['login']) ? h($_SESSION['form_data']['login']) : '';?>" required>
+                        </div>
+                        <div class="field-row">
+                            <label for="pasword">Password</label>
+                            <input type="password" name="password" class="le-input " id="pasword" placeholder="Password" data-error="Пароль должен включать не менее 6 символов" data-minlength="6" value="<?=isset($_SESSION['form_data']['password']) ? h($_SESSION['form_data']['password']) : '';?>" required>
+                        </div>
+                        <div class="field-row">
+                            <label for="name">Имя</label>
+                            <input type="text" name="name" class="le-input" id="name" placeholder="Имя" value="<?=isset($_SESSION['form_data']['name']) ? h($_SESSION['form_data']['name']) : '';?>" required>
+
+                        </div>
+                        <div class="field-row">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" class="le-input" id="email" placeholder="Email" value="<?=isset($_SESSION['form_data']['email']) ? h($_SESSION['form_data']['email']) : '';?>" required>
+
+                        </div>
+                        <div class="field-row">
+                            <label for="address">Address</label>
+                            <input type="text" name="address" class="le-input" id="address" placeholder="Address" value="<?=isset($_SESSION['form_data']['address']) ? h($_SESSION['form_data']['address']) : '';?>" required>
+
+                        </div>
+
+                        <div class="buttons-holder">
+                            <button type="submit" class="le-button huge">Зарегистрировать</button>
+                        </div><!-- /.buttons-holder -->
+                    </form>
+                    <?php if(isset($_SESSION['form_data'])) unset($_SESSION['form_data']); ?>
+                    <h2 class="semi-bold">Sign up today and you'll be able to :</h2>
+
+                    <ul class="list-unstyled list-benefits">
+                        <li><i class="fa fa-check primary-color"></i> Speed your way through the checkout</li>
+                        <li><i class="fa fa-check primary-color"></i> Track your orders easily</li>
+                        <li><i class="fa fa-check primary-color"></i> Keep a record of all your purchases</li>
+                    </ul>
+
+                </section><!-- /.register -->
+
+            </div><!-- /.col -->
+
+        </div><!-- /.row -->
+    </div><!-- /.container -->
+</main>
